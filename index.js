8ff2c6d9c30ba7a0b673af3d19fbd883e1a25310
edwin rivera <waehake@gmail.com>
2018-01-16 05:39:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "";
+var topic = "Lets talk about it .";
 
 var conversation = `${topic}`;
